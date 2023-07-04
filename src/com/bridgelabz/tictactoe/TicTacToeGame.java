a9281f87c85d2365e5b4dd855a8705b485c6c71d
Prathm <jawahireprathmesh@gmail.com>
2023-07-04 17:35:08 +0530

    UC-3 added

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -19,17 +19,25 @@ public class TicTacToeGame {
     {
         board[place]=sign;
     }
-    public static void show()
-    {
-        System.out.println(" "+board[1]+"|"+board[2]+"|"+board[3]);
-        System.out.println(" "+board[4]+"|"+board[5]+"|"+board[6]);
-        System.out.println(" "+board[7]+"|"+board[8]+"|"+board[9]);
+    public static void show() {
+        for (int i = 1; i <= 9; i += 3) {
+            System.out.println(" " + board[i] + " | " + board[i + 1] + " | " + board[i + 2]);
+            if (i < 7) {
+                System.out.println("---+---+---");
+            }
+        }
     }
+
     public static void ChooseLetter()//UC-2
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Choose the letter from 'X' or 'O:");
         char letter = sc.nextLine().charAt(0);
+    }
+    public static void showBoard()
+    {
+        System.out.println("my current board is:");
+        show();//UC-3
     }
 
     public static void main(String[] args) {
